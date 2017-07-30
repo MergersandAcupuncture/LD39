a088efa979945ee5babb37f4c2236b4e2105ce61
David Jerome <GlassToeStudio@gmail.com>
2017-07-29 23:29:21 -0500

    Added a quick and dirty camera follow script
    Also, while fixing the bug in the whietspace name the project had some
    broken prefabs and references (caused by the bug).. those missing
    references were pushed. I was not sure exaclty how the player was setup,
    but he is there, and the prefabs and script refernces are all fixed.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float smoothTimeX;
+    public float smoothTimeY;
+
+    public Transform player;
+
+
+	// Use this for initialization
+	void Start () {
+        if(player == null)
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity.x, smoothTimeX * Time.deltaTime);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.position.y, ref velocity.y, smoothTimeY * Time.deltaTime);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
